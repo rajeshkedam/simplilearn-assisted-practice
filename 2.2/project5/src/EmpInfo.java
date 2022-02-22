2a772f3818bf268701f3f66dcf06c75f20b6743e
kedamrajesh <99790870+kedamrajesh@users.noreply.github.com>
2022-02-22 16:08:55 +0530

    added changes

---
+++ 2.2/project5/src/EmpInfo.java
@@ -0,0 +1,22 @@
+
+public class EmpInfo {
+	int id;
+	String name;
+
+void display() {
+	System.out.println(id+" "+name);
+	}
+}
+
+ class constructorDemo {
+
+public static void main(String[] args) {
+
+	EmpInfo emp1=new EmpInfo();
+	EmpInfo emp2=new EmpInfo();
+
+	emp1.display();
+	emp2.display();
+	}
+
+}
